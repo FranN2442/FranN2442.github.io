1ce806407f75cfa4d7874c93972ac696ce21f609
Toni <antoniserrasastre@gmail.com>
2022-12-02 16:20:18 +0100

    Carpeta BaseDatos + funciones appJsonToHTML

---
+++ src/appJsonToHTML.py
@@ -0,0 +1,17 @@
+import json
+
+def escribirHTML(nombre, contenido):
+
+    with open("html/" + nombre + ".html", "w+") as html_file:
+        html_file.write(contenido)
+        print("HTML file created successfully")
+
+def cargarDatos(ruta='DataBase/bicisCarretera.json'):
+    with open(ruta) as contenido:
+        jsondoc = json.load(contenido)
+        mainkey = jsondoc.get('documents')
+        return mainkey
+    
+if __name__ == "__main__":
+    
+    items = cargarDatos()
