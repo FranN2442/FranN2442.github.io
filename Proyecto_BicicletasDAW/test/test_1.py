ff91ebb22302308e19ca62838e71104d1bf1128c
Toni <antoniserrasastre@gmail.com>
2022-12-12 19:44:55 +0100

    he borrado una carpeta que se me ha colado

--- Proyecto_BicicletasDAW/test/test_1.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import src.appJsonToHTML 
-
-items = src.appJsonToHTML.cargarDatos()
-
-def test_1():
-    assert src.appJsonToHTML.PaginaCategorias(items)== PaginaCategorias.html
-
-    
